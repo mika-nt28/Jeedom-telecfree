09235a1c3f7f827192214c750e6284f597defda2
Halbout Michael <michaelhalbout@gmail.com>
2019-11-28 12:21:12 +0100

    Update and rename core/telecfree.ajax.php to core/ajax/telecfree.ajax.php

--- core/ajax/telecfree.ajax.php
+++ core/ajax/telecfree.ajax.php
@@ -5,16 +5,17 @@ try {
 	if (!isConnect('admin')) {
 		throw new Exception(__('401 - Accès non autorisé', __FILE__));
 	}
-  switch(init('action')){
-  case "getChaineLogo":
-    $file="plugins/telecfree/core/template/images/chaines/".init('logicalId').".png";
-				if(file_exists($file))
-					$file
-  break;
-  default:
-  	throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
-   break;
-  }
+	switch(init('action')){
+		case "getChaineLogo":
+			$file="plugins/telecfree/core/template/images/chaines/".init('logicalId').".png";
+			if(file_exists($file))
+				ajax::success($file);
+   			ajax::error('');
+		break;
+		default:
+			throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+		break;
+	}
 	/*     * *********Catch exeption*************** */
 } catch (Exception $e) {
 	ajax::error(displayExeption($e), $e->getCode());
